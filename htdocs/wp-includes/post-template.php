43cf6020636999a1fad536c2f9f5d04174ea24b1
Alan Tian <tianheth@gmail.com>
2015-05-22 12:01:45 +1200

    added the_meta in post-template.php

--- htdocs/wp-includes/post-template.php
+++ htdocs/wp-includes/post-template.php
@@ -230,6 +230,7 @@ function the_content( $more_link_text = null, $strip_teaser = false) {
 	$content = apply_filters( 'the_content', $content );
 	$content = str_replace( ']]>', ']]&gt;', $content );
 	echo $content;
+        the_meta();
 }
 
 /**
